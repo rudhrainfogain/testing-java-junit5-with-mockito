a81025f5ce0a3f6e54c000d886ec8f4187de687f
rudhra koul <techpertitsolutions@gmail.com>
2019-03-18 00:50:43 +0530

    Returning values from mock objects demo

--- src/test/java/com/infogain/petclinic/service/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/infogain/petclinic/service/SpecialitySDJpaServiceTest.java
@@ -1,10 +1,14 @@
 package com.infogain.petclinic.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -107,5 +111,33 @@ public class SpecialitySDJpaServiceTest {
 		 * deleteById() with parameter speciality
 		 */
 		verify(specialtyRepository, times(1)).delete(speciality);
+	}
+
+	@Test
+	void findByIdTest() {
+		/*
+		 * The instance that we are going to use for return by our mock
+		 */
+		Speciality speciality = new Speciality();
+		/*
+		 * Setting when and what to return. When findById() method of our mocked object
+		 * ie specialtyRepository is called with parameter 1l then return the speciality
+		 * object created above
+		 */
+		when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+		/*
+		 * Calling the actual method under test
+		 */
+		Speciality foundSpecialty = service.findById(1L);
+		/*
+		 * asserting that an object was returned and null was not returned
+		 */
+		assertThat(foundSpecialty).isNotNull();
+		/*
+		 * Verify that specialtyRepository was called 1 times for the method findById()
+		 * with parameter of 1l
+		 */
+		verify(specialtyRepository).findById(1L);
+
 	}
 }
