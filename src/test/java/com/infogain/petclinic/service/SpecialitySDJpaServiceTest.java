64d31de13260a19ece2ce0f9c584fa15f222ef52
rudhra koul <techpertitsolutions@gmail.com>
2019-03-18 00:38:03 +0530

    Verify mock interactions demo

--- src/test/java/com/infogain/petclinic/service/SpecialitySDJpaServiceTest.java
+++ src/test/java/com/infogain/petclinic/service/SpecialitySDJpaServiceTest.java
@@ -1,5 +1,11 @@
 package com.infogain.petclinic.service;
 
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -38,15 +44,68 @@ public class SpecialitySDJpaServiceTest {
 		 * SpecialtyRepository
 		 */
 		service.deleteById(1l);
+		service.deleteById(1l);
+
+		/*
+		 * Verify that specialtyRepository was called 2 times for the method
+		 * deleteById() with parameter of 1l
+		 */
+		verify(specialtyRepository, times(2)).deleteById(1l);
+	}
+
+	@Test
+	void deleteByIdAtLeast() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		/*
+		 * Verify that specialtyRepository was called at least 1 times for the method
+		 * deleteById() with parameter of 1l
+		 */
+		verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+	}
+
+	@Test
+	void deleteByIdAtMost() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		/*
+		 * Verify that specialtyRepository was called at most 5 times for the method
+		 * deleteById() with parameter of 1l
+		 */
+		verify(specialtyRepository, atMost(5)).deleteById(1l);
+	}
+
+	@Test
+	void deleteByIdNever() {
+		service.deleteById(1l);
+		service.deleteById(1l);
+		/*
+		 * Verify that specialtyRepository was called at least 1 times for the method
+		 * deleteById() with parameter of 1l
+		 */
+		verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+		/*
+		 * Verify that specialtyRepository was never called for the method deleteById()
+		 * with parameter of 5l
+		 */
+		verify(specialtyRepository, never()).deleteById(5L);
 	}
 
 	@Test
 	void testDelete() {
+		Speciality speciality = new Speciality();
 		/*
 		 * Delete() uses an object of SpecialtyRepository which was autowired into the
 		 * class .For testing purposes we are making it to use the mocked version of
 		 * SpecialtyRepository
 		 */
-		service.delete(new Speciality());
+
+		service.delete(speciality);
+
+		/*
+		 * Verify that specialtyRepository was called 2 times for the method
+		 * deleteById() with parameter speciality
+		 */
+		verify(specialtyRepository, times(1)).delete(speciality);
 	}
 }
